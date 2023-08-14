2a3c10542409bc474917b78bd95f1a69441734c9
Daniel Theissen <84092142+FrechdachsBB@users.noreply.github.com>
2023-08-14 13:29:56 +0200

    Test Versionierung

--- bisaChatPlus.js
+++ bisaChatPlus.js
@@ -5,8 +5,8 @@
 // @author       Frechdachs
 // @match        https://community.bisafans.de/chat/index.php?room/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bisafans.de
-// @updateURL    https://raw.githubusercontent.com/FrechdachsBB/BisaChatPlus/main/version.js
-// @downloadURL  https://raw.githubusercontent.com/FrechdachsBB/BisaChatPlus/main/bisaChatPlus.js
+// @updateURL    https://raw.githubusercontent.com/FrechdachsBB/BisaChatPlus/dev/version.js
+// @downloadURL  https://raw.githubusercontent.com/FrechdachsBB/BisaChatPlus/dev/bisaChatPlus.js
 // @grant        GM.setValue
 // @grant        GM.getValue
 // ==/UserScript==
