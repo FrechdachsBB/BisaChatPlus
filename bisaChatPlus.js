73a0d335a95ca0116c14b84285042415fc4234f8
Daniel Theissen <84092142+FrechdachsBB@users.noreply.github.com>
2023-08-14 13:26:41 +0200

    Versioning geändert

--- bisaChatPlus.js
+++ bisaChatPlus.js
@@ -1,3 +1,16 @@
+// ==UserScript==
+// @name         BC+ (DEV)
+// @version      0.1
+// @description  Bloß eine schwache Imitation des ursprünglichen BC+
+// @author       Frechdachs
+// @match        https://community.bisafans.de/chat/index.php?room/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=bisafans.de
+// @updateURL    https://raw.githubusercontent.com/FrechdachsBB/BisaChatPlus/main/version.js
+// @downloadURL  https://raw.githubusercontent.com/FrechdachsBB/BisaChatPlus/main/bisaChatPlus.js
+// @grant        GM.setValue
+// @grant        GM.getValue
+// ==/UserScript==
+
 const script = async function() {
     const chatUL = document.getElementsByClassName("scrollContainer")[0].childNodes[1];
     const observerConfig = {childList: true};
--- version.js
+++ version.js
@@ -1,11 +1,12 @@
 // ==UserScript==
-// @name         BC+
+// @name         BC+ (DEV)
 // @version      0.1
 // @description  Bloß eine schwache Imitation des ursprünglichen BC+
 // @author       Frechdachs
 // @match        https://community.bisafans.de/chat/index.php?room/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bisafans.de
-// @require      https://raw.githubusercontent.com/FrechdachsBB/BisaChatPlus/main/bisaChatPlus.js
-// @grant       GM.setValue
-// @grant       GM.getValue
+// @updateURL    https://raw.githubusercontent.com/FrechdachsBB/BisaChatPlus/main/version.js
+// @downloadURL  https://raw.githubusercontent.com/FrechdachsBB/BisaChatPlus/main/bisaChatPlus.js
+// @grant        GM.setValue
+// @grant        GM.getValue
 // ==/UserScript==
